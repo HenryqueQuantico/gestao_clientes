b7a250f7cb1e025a361e4df15bff527399d1406f
sended <38009594+sended@users.noreply.github.com>
2019-07-27 18:12:42 -0300

    TemplateView

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import home, my_logout
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('', home, name="home"),
     path('logout/', my_logout, name="logout"),
+    path('home2/', TemplateView.as_view(template_name='home2.html'), name = 'tamplate_view'),
 ]
