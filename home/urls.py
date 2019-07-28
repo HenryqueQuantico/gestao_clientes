f8403a0aa43411a579bc1092717475d7aad91bbf
sended <38009594+sended@users.noreply.github.com>
2019-07-28 19:26:00 -0300

    TemplateViews Personalizadas

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import home, my_logout
 from django.views.generic import TemplateView
+from .views import HomeView
 
 
 urlpatterns = [
     path('', home, name="home"),
     path('logout/', my_logout, name="logout"),
     path('home2/', TemplateView.as_view(template_name='home2.html'), name = 'tamplate_view'),
+    path('home3/', HomeView.as_view(), name = 'tamplate_view'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.views.generic import TemplateView
 
 
 def home(request):
@@ -9,3 +10,12 @@ def home(request):
 def my_logout(request):
     logout(request)
     return redirect('home')
+
+class HomeView(TemplateView):
+    template_name = 'home3.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Variavel_Injetada'] = "Veriavel injetada no contexto da View"
+        return context
+
